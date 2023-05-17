8a47d70bbdb3ed06f286338d41262bf8538b0acc
AleLoiac <alessandroloiac@gmail.com>
2023-05-17 14:06:06 +0200

    17/05

---
+++ ex_3.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	//x := 123
+	x := 3205600
+
+	fmt.Println(reverse(x))
+
+}
+
+func reverse(x int) int {
+
+	rev := make([]int, 0)
+
+	for x > 0 {
+		num := x % 10
+		rev = append(rev, num)
+		x = x / 10
+	}
+
+	result := 0
+
+	for _, num := range rev {
+		result = result*10 + num // this conversion takes also care about the "initial 0"
+	}
+
+	if result > math.MaxInt32 {
+		return 0
+	}
+
+	if result < math.MinInt32 {
+		return 0
+	}
+
+	return result
+}
